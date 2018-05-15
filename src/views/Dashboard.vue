98c24717cbf824bfc0fbd5b0b36495b1d5f48394
joonwoo88 <jwkim@gncloud.kr>
2018-05-15 17:31:19 +0900

    dashboard design

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,88 @@
 <template>
   <div class="animated fadeIn wrapper">
+
+    <div class="row">
+      <div class="col-6 col-lg-2">
+        <div class="card">
+          <div class="card-body p-3 d-flex align-items-center">
+            <div class="bg-dark p-3 font-xl mr-3 text-white">BTC</div>
+            <div>
+            <div class="text-value-sm text-success">$8995.45 <i class="fa fa-arrow-up text-success"></i></div>
+              <div class="text-primary font-weight-bold ">1.35 btc</div>
+                <div class="text-muted small font-weight-bold ">USDT / Binance</div>
+              </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 col-lg-2">
+        <div class="card">
+          <div class="card-body p-3 d-flex align-items-center">
+            <div class="bg-dark p-3 font-xl mr-3 text-white">ETH</div>
+            <div>
+            <div class="text-value-sm text-success">$0.002 <i class="fa fa-arrow-up text-success"></i></div>
+              <div class="text-primary font-weight-bold ">1.35 eth</div>
+                <div class="text-muted small font-weight-bold ">BTC / Binance</div>
+              </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-6 col-lg-2">
+        <div class="card">
+          <div class="card-body p-3 d-flex align-items-center">
+            <div class="bg-dark p-3 font-xl mr-3 text-white">XRP</div>
+            <div>
+            <div class="text-value-sm text-success">$0.0000074 <i class="fa fa-arrow-up text-success"></i></div>
+              <div class="text-primary font-weight-bold ">0.00004 xrp</div>
+                <div class="text-muted small font-weight-bold ">BTC / Binance</div>
+              </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-6 col-lg-2">
+        <div class="card">
+          <div class="card-body p-3 d-flex align-items-center">
+            <div class="bg-dark p-3 font-xl mr-3 text-white">BNB</div>
+            <div>
+            <div class="text-value-sm text-success">$0.55 <i class="fa fa-arrow-up text-success"></i></div>
+              <div class="text-primary font-weight-bold ">20.0 bnb</div>
+                <div class="text-muted small font-weight-bold ">krw / Okcoin</div>
+              </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-6 col-lg-2">
+        <div class="card">
+          <div class="card-body p-3 d-flex align-items-center">
+            <div class="bg-dark p-3 font-xl mr-3 text-white">BNB</div>
+            <div>
+            <div class="text-value-sm text-success">$0.55 <i class="fa fa-arrow-up text-success"></i></div>
+              <div class="text-primary font-weight-bold ">20.0 bnb</div>
+                <div class="text-muted small font-weight-bold ">krw / Okcoin</div>
+              </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-6 col-lg-2">
+        <div class="card">
+          <div class="card-body p-3 d-flex align-items-center">
+            <div class="bg-dark p-3 font-xl mr-3 text-white">BNB</div>
+            <div>
+            <div class="text-value-sm text-success">$0.55 <i class="fa fa-arrow-up text-success"></i></div>
+              <div class="text-primary font-weight-bold ">20.0 bnb</div>
+                <div class="text-muted small font-weight-bold ">krw / Okcoin</div>
+              </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+
+
     <b-row>
       <b-col sm="6" lg="3">
         <SummaryRevenueCard
@@ -113,6 +196,8 @@ export default {
   },
   data () {
     return {
+      slide: 0,
+      sliding: null,
       revenueConfig: {
         height: '70',
         labels: ['2018-01-01', '2018-01-02', '2018-01-03', '2018-01-04', '2018-01-05', '2018-01-06', '2018-01-07', '2018-01-08']
@@ -145,6 +230,14 @@ export default {
         borderColor: 'rgba(255,255,255,.55)',
         data: [65, 59, 84, 84, 51, 55, 40, 80]
       }]
+    }
+  },
+  methods: {
+    onSlideStart (slide) {
+      this.sliding = true
+    },
+    onSlideEnd (slide) {
+      this.sliding = false
     }
   }
 }
