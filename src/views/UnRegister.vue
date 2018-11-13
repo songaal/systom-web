e839c19dec6d307d08e8016c856cd605b066334b
swsong <songaal@gmail.com>
2018-11-13 11:00:55 +0900

    탈퇴 ui 수정.

--- src/views/UnRegister.vue
+++ src/views/UnRegister.vue
@@ -7,17 +7,17 @@
     </b-row>
     <b-row>
       <b-col class="text-center mt-3">
-        <p>이렇게 떠나신다니 많이 아쉽습니다. 혹시라도 시간이 되신다면, 저희가 어떻게 시스텀서비스를 향상 시킬수 있을지 말씀해주세요. 모든 사람들에게 도움이 될것 입니다. 감사합니다. :)</p>
+        <p>이렇게 떠나신다니 많이 아쉽습니다. <br />혹시라도 시간이 되신다면, 저희가 어떻게 시스텀서비스를 향상 시킬수 있을지 말씀해주세요. <br /> 모든 사람들에게 도움이 될 것 입니다. <br />감사합니다. :)</p>
       </b-col>
     </b-row>
     <b-row>
       <b-col class="mt-3">
-        <b-form-textarea style="height: 200px;"></b-form-textarea>
+        <b-form-textarea rows="2"></b-form-textarea>
       </b-col>
     </b-row>
     <b-row>
       <b-col class="text-center mt-3">
-        <button class="btn btn-danger btn-lg" @click="unRegister">탈퇴합니다.</button>
+        <button class="btn btn-danger btn-lg mr-1" @click="unRegister">탈퇴합니다</button>
         <button class="btn btn-secondary btn-lg" @click="back">뒤로가기</button>
       </b-col>
     </b-row>
@@ -52,5 +52,6 @@ export default {
 </script>
 
 <style lang="css">
-.wrapper {margin-top: 20px;}
+.wrapper {margin-top: 40px; margin-bottom: 40px;}
+textarea {max-width: 600px; margin-left: auto; margin-right: auto;}
 </style>
